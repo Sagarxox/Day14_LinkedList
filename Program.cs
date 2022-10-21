8490b1e4dca157f9267da8b53eb7fb2e56723f41
sagar <skatahre@gmail.com>
2022-10-21 10:02:30 +0530

    Delete First element

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace LinkedList
 
         static void Main(string[] args)
         {
-            Console.WriteLine("please enter your choice: \n1.add number \n2.add number reverse order\n3.insert no\n");
+
+
+            Console.WriteLine("please enter your choice: \n1.add number \n2.add number reverse order\n3.insert no\n4.Append\n5.Delete First no\n");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -29,25 +31,33 @@ namespace LinkedList
                     break;
                 case 3:
                     LinkedList linkedList2 = new LinkedList();
-                    //int a=  linkedList2.Search(70);
-                    //Console.WriteLine("index position of 70 is"+a);
-                    // linkedList2.InsertNode(2, 67);
                     linkedList2.Add(56);
                     linkedList2.Add(70);
-                    linkedList2.InsertNode(2, 30);
+                    linkedList2.InsertNode(2, 12);
                     linkedList2.Display();
-
-
+                    break;
+                case 4:
+                    LinkedList linkedList3 = new LinkedList();
+                    linkedList3.Append(56);
+                    linkedList3.Append(30);
+                    linkedList3.Append(70);
+                    linkedList3.Display();
+                    break;
+                case 5:
+                    LinkedList linkedList4 = new LinkedList();
+                    linkedList4.Add(56);
+                    linkedList4.Add(30);
+                    linkedList4.Add(70);
+                    linkedList4.Pop();
+                    linkedList4.Display();
                     break;
 
             }
-
-
-
-
-
-
         }
 
+
+
     }
+
+
 }
