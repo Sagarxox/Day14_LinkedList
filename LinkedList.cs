ae152ba5c6dbd1ffa0fab1ccc93732d1a184c40b
sagar <skatahre@gmail.com>
2022-10-21 09:55:11 +0530

    insert using linked list

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,87 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinkedList
 {
+
     public class LinkedList
     {
-        public Node head;
-        public void Add(int data)
-        {
-            Node node = new Node(data);
-            if (this.head == null)
+
+       
+            public Node head;
+            public void Add(int data)
             {
-                this.head = node;
-            }
-            else
-            {
-                Node temp = head;
-                while (temp.next != null)
+                Node node = new Node(data);
+                if (this.head == null)
                 {
-                    temp = temp.next;
+                    this.head = node;
                 }
-                temp.next = node;
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into linked list", node.data);
+
             }
-            Console.WriteLine("{0} inserted into linked list", node.data);
-
-        }
-
-
-        // To Display Add Elements In Reverse Order
-
-        public void AddInReverseOrder(int data)
-        {
-            Node newNode = new Node(data);
-            if (this.head == null)
+            public void AddInReverseOrder(int data)            // To Display Add Elements In Reverse Order
             {
-                this.head = newNode;
+                Node newNode = new Node(data);
+                if (this.head == null)
+                {
+                    this.head = newNode;
+                }
+                else
+                {
+                    Node temp = this.head;
+                    head = newNode;
+                    head.next = temp;
+                }
+                Console.WriteLine("{0} inserted into linked list  ", newNode.data);
             }
-            else
+            public int Search(int value)
+            {
+                Node node = this.head;
+                int count = 0;
+                while (node != null)
+                {
+                    if (node.data == value)
+                        return count;
+                    node = node.next;
+                    count++;
+                }
+                return count;
+            }
+            public void InsertNode(int position, int data)
+            {
+                var newnode = new Node(data);
+                //newnode.next = this.head;
+                //head = newnode;
+                newnode.data = data;
+                newnode.next = this.head;
+                if (position < 1)
+                {
+                    Console.WriteLine("invalid data");
+                }
+                if (position == 1)
+                {
+                    newnode.next = this.head;
+                    head = newnode;
+                }
+                else
+                {
+                    Node node1 = new Node(data);
+                    node1 = this.head;
+
+                    while (position > 2)
+                    {
+                        node1 = node1.next;
+                        position--;
+                    }
+                   
+                    newnode.next = node1.next;
+                    node1.next = newnode;
+
+                }
+            }
+            public void Display()
             {
                 Node temp = this.head;
-                head = newNode;
-                head.next = temp;
-            }
-            Console.WriteLine("{0} inserted into linked list  ", newNode.data);
-        }
-
-        //Append        
-
-        public void Append(int data)
-        {
-            Node node = new Node(data);
-            if (head == null)
-            {
-                head = node;
-            }
-            else
-            {
-                Node node1 = head;
-                while (node1.next != null)
+                if (temp == null)
                 {
-                    node1 = node1.next;
+                    Console.WriteLine("linked list is empty");
+                    return;
                 }
-                node1.next = node;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
             }
-        }
-
-        public void Display()
-        {
-            Node temp = this.head;
-            if (temp == null)
-            {
-                Console.WriteLine("linked list is empty");
-                return;
-            }
-            while (temp != null)
-            {
-                Console.WriteLine(temp.data + " ");
-                temp = temp.next;
-            }
-        }
+        
     }
 }
 
