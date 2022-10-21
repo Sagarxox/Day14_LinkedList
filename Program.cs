e21d32eaf989436a141e583704b9a844cd1e4be7
sagar <skatahre@gmail.com>
2022-10-21 09:29:02 +0530

    Create linked list

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
