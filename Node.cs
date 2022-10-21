1d82ef5aef2a863a406fb028a55fe3dfd4cbd50f
sagar <skatahre@gmail.com>
2022-10-21 13:48:49 +0530

    delete 40 after 30

--- Node.cs
+++ Node.cs
@@ -5,6 +5,11 @@
     {
         public int data;//value of node
         public Node next;//pointer to next node
+
+        public Node()
+        {
+        }
+
         public Node(int data)//constructor
         {
             this.data = data;
