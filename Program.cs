5f99c41cb1e59f7ce3326e2df8bd6b13590d382b
sagar <skatahre@gmail.com>
2022-10-21 09:42:52 +0530

    append using linked list

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace LinkedList
 {
@@ -11,22 +7,30 @@ namespace LinkedList
         static void Main(string[] args)
         {
 
-           
-                LinkedList linkedList = new LinkedList();
-                linkedList.Add(56);
-                linkedList.Add(30);
-                linkedList.Add(70);
-                linkedList.Display();
 
 
-                LinkedList linkedList1 = new LinkedList();
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
 
-                linkedList1.AddInReverseOrder(70);
-                linkedList1.AddInReverseOrder(30);
-                linkedList1.AddInReverseOrder(56);
-                linkedList1.Display();
 
-            
+            LinkedList linkedList1 = new LinkedList();
+
+            linkedList1.AddInReverseOrder(70);
+            linkedList1.AddInReverseOrder(30);
+            linkedList1.AddInReverseOrder(56);
+            linkedList1.Display();
+
+            LinkedList linkedList2 = new LinkedList();
+
+            linkedList2.Append(56);
+            linkedList2.Append(30);
+            linkedList2.Append(70);
+            linkedList2.Display();
+
+
 
 
         }
